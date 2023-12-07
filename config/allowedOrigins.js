8d3810c76176864486ce3fbbd5370334095424d8
rhalupczok <r.halupczok@gmail.com>
2023-12-07 09:41:46 +0100

    cors update

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,9 +1,8 @@
 const allowedOrigins = [
-    "https://www.google.com",
     "http://localhost:3500",
     "http://localhost:3001",
     "http://localhost:3000",
-    "https://rhalupczok.github.io/",
+    "http://rhalupczok.github.io/",
 ];
 
 module.exports = allowedOrigins;
