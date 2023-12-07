23311ace243dd99df19f268911002b625e178865
rhalupczok <r.halupczok@gmail.com>
2023-12-07 08:39:01 +0100

    start script fix

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,6 +3,7 @@ const allowedOrigins = [
     "http://localhost:3500",
     "http://localhost:3001",
     "http://localhost:3000",
+    "https://rhalupczok.github.io/",
 ];
 
 module.exports = allowedOrigins;
